531b4bd7eaa35de8d93637ceb306e5d9495a0f8e
Luc Choubert <luc.choubert@gmail.com>
2021-09-12 22:30:09 +0200

    first version of a distributed mandelbrot over rabbitMQ

---
+++ receive_Mandelbrot.py
@@ -0,0 +1,108 @@
+## REquires to install on the machine
+# imagemagick
+# mandelbrot (From Luc Choubert)
+
+import pika, sys, os, time
+from random import randrange
+import paramiko
+
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class"""
+
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+        self._stop_time = None
+
+
+    def start(self):
+        """Start a new timer"""
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+        self._stop_time = None
+        self._start_time = time.perf_counter()
+
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+        self._stop_time = time.perf_counter()
+
+    def __str__(self):
+        elapsed_time = self._stop_time - self._start_time
+        self._start_time = None
+        self._stop_time = None
+        return "Elapsed time: {elapsed_time:0.4f} seconds"
+
+    def getElapsed_time(self):
+        elapsed_time = self._stop_time - self._start_time
+        self._start_time = None
+        self._stop_time = None
+        return elapsed_time
+
+def main():
+    ## Connect to the Job Queue RabbitMq instance to 
+    credentials = pika.PlainCredentials('mandelbrot', 'benoit')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='garage.local',virtual_host='MandelbrotGenerator',credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='task')
+
+    def callback(ch, method, properties, body):
+        ## Decode the task received
+        taskMandelbrot, targetDirectory = body.decode().split('|')
+        print("[x] Received {}".format(body.decode()))
+
+        ## Run the task received, a Mandelbrot generation
+        t = Timer()
+        t.start()
+        aReturnCodeMandelbrot = os.waitstatus_to_exitcode(os.system("nice -10 "+taskMandelbrot))
+        #aReturnCodeMandelbrot = os.WEXITSTATUS(os.system(taskMandelbrot))
+        t.stop()
+        processing_time = t.getElapsed_time()
+        
+        ## Convert the .bmp file into .jog file if mogrify exists
+        ## Another way could be via python itself and PIL module
+        imageFile = taskMandelbrot.split(' ')[7].split('.')[0]
+        taskConvertion = "nice -10 convert {0}.bmp {0}.jpg && rm -f {0}.bmp".format(imageFile)
+        aReturnCodeConvertion = os.waitstatus_to_exitcode(os.system(taskConvertion))
+        
+        ## Send the resulting image file to the storage server
+        host = "garage.local"
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname = host)
+        sftp = ssh.open_sftp()
+        aLocalPath = "./{0}.jpg".format(imageFile)
+        aRemotePath = "{0}/{1}.jpg".format(targetDirectory, imageFile)
+        sftp.put(localpath=aLocalPath, remotepath=aRemotePath)
+        ssh.close()
+
+        ## Successfully dequeue the task from the job Queue 
+        if aReturnCodeMandelbrot == 0 and aReturnCodeConvertion == 0:
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+
+        ## Report status
+        print("-->Processed in {0:.1f} seconds with RC_Mandelbrot={1} and RC_Convertion={2}".format(processing_time, aReturnCodeMandelbrot, aReturnCodeConvertion))
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='task', on_message_callback=callback)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ send_Mandelbrot.py
@@ -0,0 +1,110 @@
+import datetime
+import time
+import re
+import pika
+import paramiko
+
+IMAGE_PRECISION = 1000
+
+## Create Taskid
+uid = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+targetDirectory = "/media/BIGDATA/MandelbrotImages/{0}".format(uid)
+print("Uniq id created:{0}".format(uid))
+
+
+## Prepare Directory on File server side to receive images
+host = "garage.local"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname = host)
+sftp = ssh.open_sftp()
+sftp.mkdir(path=targetDirectory)
+#lines = sftp.listdir(path="/media/BIGDATA/MandelbrotImages")
+#print(lines)
+ssh.close()
+
+## Compute the images to build at various zoom level
+# This is where to input all the logic of going through a beautifull patch in Mandelbrot Set
+xmin = -2
+xmax = 1
+ymin = -1.5
+ymax = 1.5
+#pas = 0.000001 <- on ne voit pas le mouvement
+#pas = 0.0001 <- on ne voit pas le mouvement
+pas = 0.01
+aSequence = []
+for i in range (120):
+    aSequence.append([xmin+i*pas, xmax-i*pas, ymin+i*pas, ymax-i*pas])
+
+## Queue the job to the RabbitMq instance to distribute the load
+credentials = pika.PlainCredentials('mandelbrot', 'benoit')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='garage.local',virtual_host='MandelbrotGenerator',credentials=credentials))
+
+channel = connection.channel()
+
+#Emergency commend for queue cleanup
+channel.queue_purge(queue='task')
+
+q = channel.queue_declare(queue='task')
+i = 0
+for l in aSequence:
+    body='Message '+str(i)
+    body="Mandelbrot {xmin} {xmax} {ymin} {ymax} {nbPixel} {nbPixel} {sequence:06}-mset.bmp|{targetDir}".format(xmin = l[0], xmax = l[1], ymin = l[2], ymax = l[3], nbPixel = IMAGE_PRECISION, sequence = i, targetDir = targetDirectory)
+    channel.basic_publish(exchange='',
+                        routing_key='task',
+                        body=body)
+    #print(" [x] Sent job {0}".format(i))
+    i+=1
+print("{0} jobs queued".format(i))
+
+## Poll the job queue to detect completion 
+q = channel.queue_declare(queue='task', passive=True)
+q_len = q.method.message_count
+while q_len>0:
+    q = channel.queue_declare(queue='task', passive=True)
+    q_len = q.method.message_count
+    time.sleep(1)
+    #print("Queue Size is {0}".format(q_len))
+connection.close()
+#Workaround to make sure we have all images generated and pushed before generating the video
+time.sleep(30)
+print("Queue has been worked out")
+
+## Submit the video construction job or Build it directly, check completion, a bit of cleanup if required
+command = "cd {0} && ffmpeg -framerate 25 -i %06d-mset.jpg -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4".format(targetDirectory)
+#command = "ls {0}/*mp4".format(targetDirectory)
+ssh2 = paramiko.SSHClient()
+ssh2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh2.connect(hostname = host)
+stdin, stdout, stderr = ssh2.exec_command(command)
+print(command)
+print(stdout.readlines())
+print("Movie generated")
+
+command = "ls {0}".format(targetDirectory)
+stdin, stdout, stderr = ssh2.exec_command(command)
+lines = stdout.readlines()
+image = 0
+for f in lines:
+    aFile = f.strip()
+    #print(aFile)
+    if re.match(".*\.bmp", aFile):
+        image+=1
+    if re.match(".*\.jpg", aFile):
+        image+=1
+    if re.match(".*\.mp4", aFile):
+        print("1 video file generated: {0} from {1} image(s)".format(aFile, image))
+if not lines:
+    command = "rmdir {0}".format(targetDirectory)
+    ssh2.exec_command(command)
+    print("No image file generated, cleaning up directory")
+
+#Workaround to avoid unexpected exception when closing the ssh connection
+time.sleep(5)
+
+try:
+    ssh2.close()
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+
+
