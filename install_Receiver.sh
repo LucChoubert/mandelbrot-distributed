8ba63c9eeecfcf7215dec64b05ec0ecf64eb8359
Luc Choubert <luc.choubert@gmail.com>
2021-09-18 16:14:42 +0200

    Improved install process

--- install_Receiver.sh
+++ install_Receiver.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
-INSTALL_DIR=$HOME/GitHub/mandelbrot-distributed-TOREMOVE
+HOSTNAME=`hostname`
 
-mkdir -p $INSTALL_DIR
-cd $INSTALL_DIR
-ls
+if [ "$HOSTNAME" = "Luc-Ubuntu" ]; then
+    echo "Should not run in that machine, Exiting"
+    exit 1
+else
+
+    BASE_DIR=$HOME/AUTOMATION
+    mkdir -p $BASE_DIR
+    cd $BASE_DIR
+    rm -rf mandelbrot-distributed
+
+    git clone https://github.com/LucChoubert/mandelbrot-distributed.git
+
+    INSTALL_DIR=$BASE_DIR/mandelbrot-distributed
+    cd $INSTALL_DIR
+    python3 -m venv .env
+    source .env/bin/activate
+    pip3 install -r requirements.txt
+fi
--- run_Receiver.sh
+++ run_Receiver.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+INSTALL_DIR=$HOME/AUTOMATION/mandelbrot-distributed
+
 NBPROC=`nproc --all`
 
 for i in $(seq 1 $NBPROC);
  do
-   tmp_file="$(mktemp /tmp/myscript.XXXXXX)"
+   tmp_file="$(mktemp /tmp/receive_Mandelbrot.XXXXXX)"
    echo "$i. Running Mandelbrot Receiver and logging on $tmp_file"
-   nohup /home/luc/GitHub/mandelbrot-distributed/.venv/bin/python /home/luc/GitHub/mandelbrot-distributed/receive_Mandelbrot.py >$tmp_file 2>&1 &
+   nohup nice -10 $INSTALL_DIR/.venv/bin/python $INSTALL_DIR/receive_Mandelbrot.py >$tmp_file 2>&1 &
 done
