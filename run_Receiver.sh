75f7d3fb26623ae91f71afc77ed71d59e8c538cd
Luc Choubert <luc.choubert@gmail.com>
2021-09-19 14:02:13 +0200

    do some cleanup of logs and processes before restarting

--- run_Receiver.sh
+++ run_Receiver.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
+## Run the mandelbrot dequeurs as many as there are core on the machines
+## Kill all of the running one first and clean the previous log files
 
 INSTALL_DIR=$HOME/AUTOMATION/mandelbrot-distributed
 
 NBPROC=`nproc --all`
+
+pkill -f receive_Mandelbrot.py
+rm -f /tmp/receive_Mandelbrot.*
 
 for i in $(seq 1 $NBPROC);
  do
