440e1da7b06af1a86ed6fc6e2753aa42928a7e26
Luc Choubert <luc.choubert@gmail.com>
2021-09-19 14:34:20 +0200

    Bug fixes

---
+++ DeployAndRunAllNodes.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+## Install and Run on all nodes
+
+for m in luc@alex-ubuntu1.local luc@lilian-ubuntu1.local
+do
+
+    ssh $m "bash -s" -- < /home/luc/GitHub/mandelbrot-distributed/install_Receiver.sh 
+    ssh $m "$HOME/AUTOMATION/mandelbrot-distributed/run_Receiver.sh"
+
+done
--- install_Receiver.sh
+++ install_Receiver.sh
@@ -6,33 +6,32 @@ HOSTNAME=`hostname`
 
 if [ "$HOSTNAME" = "Luc-Ubuntu" ]; then
     echo "Should not run in that machine, Exiting"
-    ssh alex-ubuntu1.local:/home/luc/AUTOMATION/mandelbrot-distributed/run_Receiver.sh
     exit 1
 else
-
+    echo "## Running deployment on $HOSTNAME ##"
     pkill -f receive_Mandelbrot.py
 
     BASE_DIR=$HOME/AUTOMATION
     mkdir -p $BASE_DIR
     cd $BASE_DIR
 
-    rm -rf mandelbrot-cpp
-
+    INSTALL_DIR=$BASE_DIR/mandelbrot-cpp
+    rm -rf $INSTALL_DIR
+    cd $BASE_DIR
     git clone https://github.com/LucChoubert/mandelbrot-cpp.git
  
-    INSTALL_DIR=$BASE_DIR/mandelbrot-cpp
+    
     cd $INSTALL_DIR
     make clean
     make build
     make install
 
 
-
-    rm -rf mandelbrot-distributed
-
+    INSTALL_DIR=$BASE_DIR/mandelbrot-distributed
+    rm -rf $INSTALL_DIR
+    cd $BASE_DIR
     git clone https://github.com/LucChoubert/mandelbrot-distributed.git
 
-    INSTALL_DIR=$BASE_DIR/mandelbrot-distributed
     cd $INSTALL_DIR
     python3 -m venv .venv
     source .venv/bin/activate
